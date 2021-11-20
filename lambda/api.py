0f1afecaa2e97c28c59a8572ce928e877498786b
Christoph Becker <mail@ch-becker.de>
2021-11-20 13:09:55 +0100

    clarification

--- lambda/api.py
+++ lambda/api.py
@@ -7,18 +7,19 @@ from main import reverse_it
 
 def handler_name(event, context):
     try:
-
+        result = ''
         content_type_header = event['headers']['content-type']
-        postdata = base64.b64decode(event['body']).decode('iso-8859-1')
-        for part in decoder.MultipartDecoder(postdata.encode('utf-8'), content_type_header).parts:
+        post_data = base64.b64decode(event['body']).decode('iso-8859-1')
+        for part in decoder.MultipartDecoder(post_data.encode('utf-8'), content_type_header).parts:
             result = reverse_it(part.text.split('\n'))
-        result = '\n'.join(result)
+            result = '\n'.join(result)
         return result
     except Exception as e:
         print('error occured', e)
         print('# event')
         print(event)
 
+    # just for demo purposes. This does not make much sense here :) I'd be better not to catch all exceptions!
     return {
         "statusCode": 500,
         "body": "could not handle error, see log",
